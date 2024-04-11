9e34ca4cfdaf2e2450677ba385b49c3558d054cc
ArturBDev <arturbernardogithub@gmail.com>
2024-04-11 13:13:40 -0300

    trying to fizx h-screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,10 @@ import Menu from "../components/Menu";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className="h-screen">
       <Menu />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
 
